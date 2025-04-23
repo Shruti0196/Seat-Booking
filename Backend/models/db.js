92e238f0b7b5cfc16a1696356d90a07e7ea4a774
Shruti Prasad <shrutiap2003@gmail.com>
2025-04-23 17:10:41 +0530

    Code cleanup

--- Backend/models/db.js
+++ Backend/models/db.js
@@ -1,25 +1,3 @@
-// import pkg from 'pg';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-// const { Pool } = pkg;
-
-// // const { Pool } = require('pg');
-
-// // Use DATABASE_URL from the environment variable
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false, // For Render-hosted DBs, use SSL
-//     },
-// });
-
-// pool.query('SELECT NOW()', (err, res) => {
-//     if (err) {
-//       console.error('Database connection failed', err);
-//     } else {
-//       console.log('Database connected at:', res.rows[0].now);
-//     }
 import pkg from 'pg';
 import dotenv from 'dotenv';
 
